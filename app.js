2934c377b6b5eb85581ba8889c4e7faec9b8050b
nehachikode <54356232+nehachikode@users.noreply.github.com>
2021-08-10 23:33:07 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var createError = require('http-errors');
+var express = require('express');
+var logger = require('morgan');
+var dbConnect = require('./lib/common/dbConfig');
+
+var app = express();
+
+var port = Number(process.env.PORT || 3000);
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// database connection
+dbConnect();
+
+require('./lib/Router')(app);
+
+app.get('/', (req, res) => {
+  res.send(`<h2>Welcome!!!</h2>`)
+});
+
+app.listen(port, function () {
+  console.log("Server listening on port " + port);
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ test/record.js
@@ -0,0 +1,116 @@
+process.env.NODE_ENV = 'test';
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+let server = require('../app');
+const mongoose = require('mongoose');
+
+const recordController = require('../lib/controllers/record');
+const { expect } = require('chai');
+
+beforeEach(async function () {
+    await mongoose.connect(process.env.TEST_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+});
+
+describe('Records', () => {
+
+    describe('fetch', () => {
+        it('should validate correctly with proper request', () => {
+            const record = {
+                "startDate": "2016-01-26",
+                "endDate": "2018-02-02",
+                "minCount": 2000,
+                "maxCount": 2300
+            };
+            chai.request(server)
+                .post('/records/fetch')
+                .send(record)
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('object');
+                    expect(res.body.code).to.be.equal(0);
+                    expect(res.body.msg).to.be.equal('Success');
+                    expect(res.body.records).to.be.an('array');
+                });
+        });
+
+        it('should reject if request body is empty', () => {
+            const record = {};
+
+            chai.request(server)
+                .post('/records/fetch')
+                .send(record)
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    expect(res.body).to.be.an('object');
+                    expect(res.body.code).to.be.equal(1);
+                    expect(res.body.msg).to.be.equal('Invalid request');
+                    expect(res.body.errors).to.be.an('array');
+                });
+        });
+
+        it('should reject if start/end date is not in format', () => {
+            const record = {
+                "startDate": new Date("06-01-2026").toISOString(),
+                "endDate": "2018-02-02",
+                "minCount": 2000,
+                "maxCount": 2300
+            };
+
+            chai.request(server)
+                .post('/records/fetch')
+                .send(record)
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    expect(res.body).to.be.an('object');
+                    expect(res.body.code).to.be.equal(1);
+                    expect(res.body.msg).to.be.equal('Invalid request');
+                    expect(res.body.errors).to.be.an('array');
+                    expect(res.body.errors[0]).to.be.equal('Invalid Start Date')
+                });
+        });
+
+        it('should reject if min/max count is less that 0', () => {
+            const record = {
+                "startDate": "2016-01-26",
+                "endDate": "2018-02-02",
+                "minCount": -1,
+                "maxCount": 2300
+            };
+
+            chai.request(server)
+                .post('/records/fetch')
+                .send(record)
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    expect(res.body).to.be.an('object');
+                    expect(res.body.code).to.be.equal(1);
+                    expect(res.body.msg).to.be.equal('Invalid request');
+                    expect(res.body.errors).to.be.an('array');
+                    expect(res.body.errors[0]).to.be.equal('Minimum/Maximum count should be greater than 0');
+                });
+        });
+
+        it('should reject if max count is less that min', () => {
+            const record = {
+                "startDate": "2016-01-26",
+                "endDate": "2018-02-02",
+                "minCount": 2000,
+                "maxCount": 230
+            };
+
+            chai.request(server)
+                .post('/records/fetch')
+                .send(record)
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    expect(res.body).to.be.an('object');
+                    expect(res.body.code).to.be.equal(1);
+                    expect(res.body.msg).to.be.equal('Invalid request');
+                    expect(res.body.errors).to.be.an('array');
+                    expect(res.body.errors[0]).to.be.equal('Maximum Count Must Be Greater Than Minmum Count');
+                });
+        });
+    })
+});
